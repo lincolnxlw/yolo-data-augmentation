8ddacd5fb5a71700939ae9dde21ea6a66e336e85
lincolnxlw <lincolnxlw@gmail.com>
2024-09-25 03:14:28 -0500

    Add test mode

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import argparse
 from tqdm import tqdm
 
 
-def run_yolo_augmentor(need_save_bb_image=False):
+def run_yolo_augmentor(need_save_bb_image=False,
+                       is_test=False,
+                       test_num=0):
     """
     Run the YOLO augmentor on a set of images.
 
@@ -14,6 +16,9 @@ def run_yolo_augmentor(need_save_bb_image=False):
     imgs = [img for img in os.listdir(CONSTANTS["inp_img_pth"]) if is_image_by_extension(img)]
 
     for img_num, img_file in enumerate(tqdm(imgs, mininterval=1.0)):
+        if is_test and img_num >= test_num:
+            break
+
         print(f"Processing image {img_num+1}: {img_file}")
         image, gt_bboxes, aug_file_name = get_inp_data(img_file)
         aug_img, aug_label = get_augmented_results(image, gt_bboxes)
@@ -25,10 +30,18 @@ def run_yolo_augmentor(need_save_bb_image=False):
             if len(aug_label) == 0:
                 print(f"Augmented label is empty for {img_file}")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the YOLO augmentor on a set of images.")
     parser.add_argument('--need_save_bb_image', action='store_true', help='Save bounding box images.')
+    parser.add_argument('--is_test', action='store_true', help='Test mode')
+    parser.add_argument('--test_num', type=int, default=0, help='Number of images to test')
     args = parser.parse_args()
-    need_save_bb_image = args.need_save_bb_image
 
-    run_yolo_augmentor(need_save_bb_image=need_save_bb_image)
+    need_save_bb_image = args.need_save_bb_image
+    is_test = args.is_test
+    test_num = args.test_num
+
+    run_yolo_augmentor(need_save_bb_image=need_save_bb_image,
+                       is_test=is_test,
+                       test_num=test_num)
